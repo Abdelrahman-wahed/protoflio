5396dddbda353cb0ebd1eea5451ec98ce6d690a4
Abdelrahman Waheed <wabdo5242@gmail.com>
2024-10-15 00:41:33 +0300

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+// import bootstrap from 'bootstrap/dist/css/bootstrap.css'
+import './App.css';
+
+import Header from './Components/1-Header/Header';
+import Hero from './Components/2-Hero/Hero';
+import Main from './Components/3-MainContent/Main';
+import Contact from './Components/4-Contact/Contact';
+import Footer from './Components/5-Footer/Footer';
+import About from './Components/About/About';
+import Skills from './Components/Skills/Skills';
+
+
+function App() {
+  return <>
+   <div className='container1 px-2'>
+  <Header/>
+  <div className='divider' />
+  <Hero/>
+  <div className='divider' />
+  <About/>
+  <div className='divider' />
+  <Skills/>
+  <div className='divider' />
+  <Main/>
+  <div className='divider' />
+  <Contact/>
+  <div className='divider' />
+  </div>
+  <Footer/>
+  </>
+  
+  
+}
+
+export default App;
